da531544853b02c8b93ada8a15b3200cc56155b4
didies <didi_marielitos@hotmail.com>
2016-02-19 09:23:17 -0600

    Update calc_depth.c

--- calc_depth.c
+++ calc_depth.c
@@ -8,7 +8,7 @@
 #include "utils.h"
 #include <math.h>
 #include <stdio.h>
-int sqr(int a);
+int distan_eucli(int a, int b, int c);
 
 unsigned char normalized_displacement(int, int, int);
 
@@ -25,66 +25,63 @@ unsigned char normalized_displacement(int dx, int dy,
 void calc_depth(unsigned char *depth_map, unsigned char *left,
                 unsigned char *right, int image_width, int image_height,
                 int feature_width, int feature_height, int maximum_displacement) {
-	int l = 0; 
-	int m = 0;
-	char featurel[2*feature_width+1][2*feature_height+1];
-	char featurer[2*feature_width+1][2*feature_height+1];
-	for (int j2 = 0; j2<2*feature_height+1; j2++){
-		for(int j = 0; j<2*feature_width+1; j++){
-			l = l + j;
-			featurel[j2][j]= *(left+l); 
-		}
-		l = l +image_width;
-	}
+	//int es = 0;
+	//int a1 = 0; 
+	int top = 0;
+	char featurel [2*feature_width+1] [2*feature_height+1];
+	//char featurer [2*feature_width+1] [2*feature_height+1];
+	
+	// 0 en depth.
+	for(int i = 0; i<image_width; i++){ *(depth_map+i) = 0; }
+	for(int y = image_height*image_width; y>(image_width*image_height)-image_width; y--){ *(depth_map+y) = 0; }
+	for(int j = 0; j<image_width*image_height; j= j + image_width){ *(depth_map+j) = 0; *(depth_map+(j-1)) = 0; }
 
-	for (int k2 = 0; k2<2*feature_height+1; k2++){
-		for(int k = 0; k<2*feature_width+1; k++){
-			m = m + k;
-			featurer[k2][k]= *(right+m); 
-		}
-		m = m +image_height;
-	}
-
-	for(int u = 0; u<2*feature_height+1; u++){
-		for(int u2 = 0; u2 <2*feature_width+1; u2++){
-			while(featurel[u][u] != featurer[u][u2]){
-				*(depth_map+u2) = 0; 
-			}
-			*(depth_map+u2)= 255;	
-		}
-	}
-	/*for(i= 0; i < image_width*image_height; i++){
-		*(depth_map+i) = 0;
-			if(i == k){d
-				for (j = 11; j>0; j--){
-					//printf("i=%d\nj=%d\n%d\n",i,j,*(depth_map+(i-j)));
-					*(depth_map+(i-j))=255;
-					if (left+0 == right+(i-j)){
-						printf("%s\n","yeih");
+	// x(1,1).
+	//parche left.
+	for(int k = 1+image_width; k<image_width*image_height; k++){
+		//int top = k-1-image_width;
+		for(int q = 1; q<image_height; q++){
+			for(int w = 0; w<(2*feature_width+1); w++){
+				for(int x= 0; x<(2*feature_width+1); x++){
+					for(int x= 0; x<(2*feature_width+1); x++){
+						featurel[w][x] = *(left+(top+x));
+						featurer[w][x] = *(right+(top+x));		
+						*(depth_map+k) = 255;
 					}
 				}
-			k = k + image_width;
+			top = top + image_width; 
 			}
-	}*/
-/*
-					for(j=0;j<section_width;j++){
-					        for(i=0;i<section_width;i++){
-					            //pos+section_width 
-					            if(t==*(depth_map+pos+i)){
-					                t=*(depth_map+pos+i);
-					            }else{
-					                return 256;
-					            }
-					        }
-					        pos=pos+map_width;
-					    }
-*/
-		//for(int o = 0; o<50 = 0++)
-		//	*(depth_map+o) = 128; 
-		//	for(int u = 0; u <50 )
-		/* YOU CODE HERE*/
+		}
+	}
 }
-int distan_eucli(char a[], char b[], int len){
+		/*
+		//	if (((k-image_width > 0) && (q-image_height > 0)) && (((k-image_width)+2*feature_width+1 < image_width) && ((q-image_height)+2*feature_height+1 < image_height))){
+				
+				//	a1 = featurel[w][x];
+						b1 = featurer[w][w]; 
+						es = distan_eucli(a1, b1, 1);
+						if(es != 0){
+							*(depth_map)= 0; 
+						}
+						if(es == 0){
+							*(depth_map) = 128;
+					}
+				}
+			}
+		//}
+		top = top+image_width;
+	//}
+}
+}
+*/
+int distan_eucli(int k, int m, int c){
+		int res = ((k-m)*(k-m)); 
+		if (res == 0){
+			return 0;
+		}
+	return res;
+}
+/*int distan_eucli(char a[], char b[], int len){
 	int i; 
 	char k, m;
 	int res = 0;
@@ -94,4 +91,4 @@ int distan_eucli(char a[], char b[], int len){
 		res = res + ((k-m)*(k-m)); 
 		}
 	return res;
-}
+}*/
