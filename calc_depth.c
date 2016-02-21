6c655278b60c6571ef2b444d5dffa0c0c4bb87a1
didies <didi_marielitos@hotmail.com>
2016-02-20 19:59:22 -0600

    Update calc_depth.c

--- calc_depth.c
+++ calc_depth.c
@@ -26,8 +26,10 @@ void calc_depth(unsigned char *depth_map, unsigned char *left,
                 int feature_width, int feature_height, int maximum_displacement) {
 	int es = 0;
 	int a = 666666666; 
-	int topl = 0;
-	int topr = 0;
+	//int topl = 0;
+	//int topr = 0;
+	char pop= 0; 
+	char m = 0; 				
 	char featurel [2*feature_width+1] [2*feature_height+1];
 	char featurer [2*feature_width+1] [2*feature_height+1];
 	int maxab = maximum_displacement*image_width; 
@@ -42,73 +44,49 @@ void calc_depth(unsigned char *depth_map, unsigned char *left,
 	for(int j = 0; j<image_width*image_height; j= j + image_width){ *(depth_map+j) = 0; *(depth_map+(j-1)) = 0; }
 	// x(1,1).
 	//parche left.
-	for(int k = 1+image_width; k<image_width*image_height; k++){
-		topl = image_width+1;
-		topr = image_width+1;		
-		//
-		//for(int q = 1; q<image_height; q++){
-			//if (((k-image_width > 0) && (q-image_height > 0)) && (((k-image_width)+2*feature_width+1 < image_width) && ((q-image_height)+2*feature_height+1 < image_height))){
-				for(int w = 0; w<(2*feature_width+1); w++){
-					for(int x= 0; x<(2*feature_width+1); x++){
-						featurel[w][x] = *(left+(topl+x));
-						//printf("%c\t", featurel[w][x]);
-						//
+	for(int k = image_height; k<image_width*image_height; k = k + image_width){
+		for(int q = 1; q<image_width; q++){
+			if(((k-(2*feature_width+1*image_height)>0) && (q-2*feature_width+1 > 0)) || ((k+(2*feature_height+1*image_width) < image_width*image_height) && ((q+2*feature_width)<image_width))){
+				for(int w = 0; w<2*feature_width+1; w++){
+					for(int x= 0; x<2*feature_width+1; x++){
+						featurel[w][x] = *(left+(k+q));
 					}
-					topl = topl + image_width;
 				}
-
-
-				//while(){//mientras q sea 0.
-				//max_dis  //x entre +-max dis AND y entre +- maxdis
-/**/				 
-				if(k < maxab){
-					tama = maxab -(maxab - k);}
-				if(k+maxab > image_width*image_height){
-					tamb =  image_width*image_height- (k - maxab);} 
-				if(k-image_width < maxlr){
-					temi = maxlr - (maxlr -(k-image_width));} 
-				if(k+maxlr > image_height){
-					temd = image_height - (k-image_width);}
-				
-				for(int gg =k-tama-1; gg<tamb; gg= gg + image_width){
-					
-					for(int gg2 = (k-image_width)-temi; gg2<temd; gg2++){
-						*(depth_map+k) = 255;
+				//parche de right.
+				if(k < maxab){ tama = maxab -(maxab - k); }
+				if(k+maxab > image_width*image_height){ tamb =  image_width +(image_width*image_height-k-maxab); } 
+				if(q < maxlr){ temi = maxlr - (maxlr-q);} 
+				if(k+maxlr > image_height){temd = maxlr + (image_width-q-maxlr);}
+				for(int gg = k-tama; gg<tamb+tamb+image_width;  gg= gg + image_width){	
+					for(int gg2 = q-temi; gg2<temd+temi; gg2++){
 						for(int y = 0; y<2*feature_width+1; y++){
 							for(int x=0; x<2*feature_height+1;x++){
-								featurer[y][x] = *(right+(topr+x));
-								//printf("%d\t", featurer[y][x]);
-
+								featurer[y][x] = *(right+(gg+gg2));
 							}
 						}
-
-						char pop= 0; 
-						char m = 0; 
-						int jj3;
 						for(int ii = 0; ii < 2*feature_width+1; ii++){
-							for(jj3= 0; jj3< 2*feature_height+1; jj3++){
-							pop = featurel[ii][jj3];
-							m = featurer[ii][jj3];
-							es = es + ((pop-m)*(pop-m)); 
-							//printf("%d\t", es);
-							if(es < a){
-								a = es;
-							//if(es == 0){
-							//	*(depth_map+k) = 0;
-							//}
-							if(es == 0){
-								*(depth_map+k) = 128;
+							for(int jj3= 0; jj3< 2*feature_height+1; jj3++){
+								pop = featurel[ii][jj3];
+								m = featurer[ii][jj3];
+								es = ((pop-m)*(pop-m)); 
+								if(es == 0){
+									*(depth_map+(k+q)) = es;
+								}
+								//if(es != 0){
+								//	*(depth_map+(k+q)) = es;
+								//}
 							}
 						}
-					
-				
-						//features llenas :v
-						//ya calcula distanc
-			}	//comparar dEclda
-		}// /max_dis
-		topr = topr + image_width;
+					}
+				}/*
+							if(es < a){
+								//printf("%d\t", es);
+								//
+								*(depth_map+k) = 255;
+								a = es;
+				*/
+				//k = k + image_height;		
 			}
-		//}
-	//}
-}
+		}
+	}
 }
